0fdd6ae5f4f486f2a8b5f7dad7888f16ac079d92
CCOviedo <37302280+CCOviedo@users.noreply.github.com>
2019-10-21 16:36:52 +0200

    Add files via upload

---
+++ ejercicio-8a.js
@@ -0,0 +1,7 @@
+var cadena = "Amigo";
+
+alert(cadena.toLowerCase());
+alert(cadena.replace("A","a"));
+alert("a"+cadena.slice(1,5)); //tambien podria utilizarse alert("a"+cadena.slice(1,cadena.length));
+
+
