69d5690fa541ab5bc3324365910b8fbaed4e4421
CCOviedo <37302280+CCOviedo@users.noreply.github.com>
2019-10-21 16:37:43 +0200

    Delete ejercicio-8a.js

--- ejercicio-8a.js
+++
@@ -1,7 +0,0 @@
-var cadena = "Amigo";
-
-alert(cadena.toLowerCase());
-alert(cadena.replace("A","a"));
-alert("a"+cadena.slice(1,5)); //tambien podria utilizarse alert("a"+cadena.slice(1,cadena.length));
-
-
